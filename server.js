9b5aa621459d3678da953d0e72bbc264a1ea6253
sandicc <alexanderkoza97@gmail.com>
2019-09-21 21:41:06 +0200

    finished registration

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const Client = require('pg').Client;
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
+
+app.use(cors());
+app.use(express.json());
+
 
 const client = new Client({
   user: 'alex',
@@ -10,6 +18,10 @@ const client = new Client({
   password: 'b0bmba22',
   port: 5432,
 })
+
+let milist_last = Date();
+let flag = false;
+
 
 client.connect();
 
@@ -58,27 +70,38 @@ let onlineUsers = [
 ]
 
 
-let update = {
-    messages: messages,
-    onlineUsers: onlineUsers
+const update = () => {
+    if(flag) return;
+    flag = true;
+    client.query(`SELECT * FROM chat_log;`)
+        .then(res => {
+            console.log(res.rows)
+            io.emit('update', res.rows);  
+        })
+        .catch(err => console.log('Database error!'))
+        .finally(() => flag = false);
 }
-
 const getMessages = () => {
-    client.query("SELECT * FROM chat_log;")
+    client.query(`SELECT * FROM chat_log WHERE time >= ${milist_last};`)
         .then(res => console.log(res.rows))
-        .catch(err => console.log('Database Error!'));
+        .catch(err => console.log('Database error!'));
 }
 
-getMessages();
 
 app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
+app.post('/register', function (req, res) {
+    const {username, email, password} = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+    client.query(`INSERT INTO login (username, email, hash) VALUES ('${username}', '${email}', '${hash}');`)
+        .then(response => res.json({insertSuccesful: true}))
+        .catch(err => res.status(400).json({insertSuccesful: false}));
+})
+
 io.on('connection', (socket) => {
     console.log('user connected');
-    socket.on('authentication', (data) => {
-    });
     socket.on('disconnect', (reason) => {
         console.log('user has disconnected');
     });
@@ -86,5 +109,5 @@ io.on('connection', (socket) => {
    
 http.listen(3001, function(){
     console.log('listening on *:3001');
-    setInterval(() => io.emit('update', update), 5000);
+    // setInterval(update, 500);       odkomentiraj ko bo delalo --- posilja upadte na dolocen interval
 });
